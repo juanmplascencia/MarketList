cc6e118b874d2e3633e5808832637513f082bc59
Toro Roan <torodomo@me.com>
2017-10-24 15:11:35 -0700

    add login method

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,15 +3,48 @@ let User = mongoose.model('users');
 let path = require('path');
 
 module.exports = {
-    session: function(req, res){
-        if(req.session.user_id){
-            return res.json({
-                status: true,
-                user_id: req.session.user_id
-            })
-        }
-        return res.json({ status: false })
+    login: function (req, res) {
+        User.findOne({name: req.body.name}, (err, user) => {
+			if(err){
+				return res.status(401).json(err)
+			}
+			else if(user){
+				req.session.user = user
+				res.json({user: user})
+			}
+			else{
+				let user = new User(req.body);
+				user.save((err) => {
+					if(err){
+						return res.status(401).json(err);
+					}
+					else{
+						console.log(`${user} has been saved`)
+						req.session.user = user;
+						res.json({user: user});
+					}
+				})
+			}
+		})
     },
+    // This method finds the user in the database and if they exist, then
+    // it'll log them in and store the name in session
+    // If they don't exist, it'll create a new user in the database and then
+    // save their name in session
+
+    session: function (req, res) {
+		if(req.session.user){
+			return res.json(req.session.user);
+		}
+		else{
+			return res.status(500).json("Not logged in")
+		}
+	},
+    // This method is only run after the user is logged in.
+    // Thus the name is already stored in session
+    // So, this method just grabs the name from session to be
+    // used in our front end
+
     index: function(req, res){
         User.find({}, function(err, users){
             if(err){
@@ -20,6 +53,8 @@ module.exports = {
             return res.json(users);
         })
     },
+    // This method grabs all the polls and sends them off
+
     create: function(req, res){
         User.create(req.body, function(err, user){
             if(err){
@@ -29,6 +64,7 @@ module.exports = {
             return res.json(user);
         })
     },
+
     show: function(req, res){
         User.findById(req.params.id).populate('items').exec(function(err, user){
             if(err){
@@ -37,6 +73,7 @@ module.exports = {
             return res.json(user);
         })
     },
+
     authenticate: function(req, res){
         User.findOne({ email: req.body.email }, function(err, user){
             if(err){
@@ -55,6 +92,7 @@ module.exports = {
             })
         })
     },
+
     logout: function(req, res){
         if(req.session.user_id){
             delete req.session.user_id
