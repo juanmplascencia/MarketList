3ac644d74bcacecc567fe3519f357f5f29f8dc2a
Toro Roan <torodomo@me.com>
2017-10-25 12:31:20 -0700

    added item service, some components

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @Output() createUserEvent = new EventEmitter;
+  loginUser = {};
+  newUser = {};
+  registrationErrors = [];
+  loginErrors = [];
 
-  constructor() { }
+  constructor(
+    private _userService: UserService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  login(loginUser) {
+    console.log(loginUser);
+    return this._userService.authenticate(loginUser)
+    .then(user => {
+      console.log(user);
+      if (user.errors) {
+        console.log('here');
+        for (let key in user.errors) {
+          let error = user.errors[key];
+          this.loginErrors.push(error.message);
+          console.log(this.loginErrors);
+        }
+      } else {
+          this._userService.storeCurrentUser(user);
+          this._router.navigateByUrl('/dashboard');
+      }
+     })
+    .catch(err => { console.log(err);
+    });
+  }
+
+  createUser(newUser) {
+    console.log(newUser);
+    return this._userService.create(newUser)
+    .then(user => {
+      console.log(user);
+      if (user.errors) {
+        for (let key in user.errors) {
+          let error = user.errors[key];
+          this.registrationErrors.push(error.message);
+        }
+      } else {
+        this._userService.storeCurrentUser(user);
+        this._router.navigateByUrl('/dashboard');
+      }
+    })
+    .catch(err => { console.log(err);
+    });
+  }
 }
--- client/src/app/user.service.ts
+++ client/src/app/user.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs';
 
 @Injectable()
 export class UserService {
 
-  constructor() { }
+  constructor(private _http: Http) { }
 
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  storeCurrentUser(user) {
+    localStorage.setItem('currentUser', JSON.stringify(user));
+  }
+
+  create(newUser) {
+    return this._http.post('/users', newUser)
+    .map(data => data.json())
+    .toPromise();
+  }
+
+  show(id: number) {
+    return this._http.get(`/users/${id}`)
+    .map(data => data.json())
+    .toPromise();
+  }
+
+  session() {
+    return this._http.get('/session')
+    .map(data => data.json())
+    .toPromise();
+  }
+
+  authenticate(loginUser) {
+    console.log(loginUser);
+    return this._http.post('/session', loginUser)
+    .map(data => data.json())
+    .toPromise();
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+  }
 }
