ec1e19d2f6a1fc2597d2684b6d1789322afa24d2
Juan <plascencia.juan831@gmail.com>
2017-10-26 22:42:30 -0700

    add image url

--- client/src/app/browse/browse.component.ts
+++ client/src/app/browse/browse.component.ts
@@ -15,7 +15,7 @@ export class BrowseComponent implements OnInit {
   currentUser = {};
   item_error = [];
   destroy_error = [];
-
+  default = "https://renderman.pixar.com/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png";
   constructor(
     private _itemService: ItemService,
     private _userService: UserService,
